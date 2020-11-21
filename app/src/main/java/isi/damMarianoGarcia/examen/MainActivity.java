bd8400e964ffd8b90216efc6729ae10d7f6098f8
nmgarcia <nemarianogarcia@gmail.com>
2020-11-20 21:26:25 -0300

    Nos vemos en el recuperatorio :(

--- app/src/main/java/isi/damMarianoGarcia/examen/MainActivity.java
+++ app/src/main/java/isi/damMarianoGarcia/examen/MainActivity.java
@@ -3,12 +3,26 @@ package isi.damMarianoGarcia.examen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnIrAMapa;
+    EditText editTextNombre;
+    RadioGroup radioGroupGenero;
+    RadioButton masculino,femenino,otro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIrAMapa = findViewById(R.id.btnIrAMapa);
+        editTextNombre = findViewById(R.id.nombre);
+        radioGroupGenero = findViewById(R.id.radiogroupgenero);
+
     }
 }
